13726f64986008cd8603b43fe474a8500f0f75f5
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-11 11:03:25 -0400

    Nuestro primer componente

---
+++ React De cero a experto ( Hooks y MERN )/03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,12 @@
+// export function App() {
+//   // document.createElement
+//   return <h1>Hola mundo</h1>;
+// }
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+};
