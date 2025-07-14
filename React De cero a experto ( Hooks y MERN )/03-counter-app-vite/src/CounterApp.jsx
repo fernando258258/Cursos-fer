29ce9411cad438e4ffd2a658d8186910f03e32a2
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-14 10:27:10 -0400

    Evento click (Eventos en general)

--- React De cero a experto ( Hooks y MERN )/03-counter-app-vite/src/CounterApp.jsx
+++ React De cero a experto ( Hooks y MERN )/03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+  const handleAdd = (event) => {
+    console.log(event);
+  };
+
   return (
     <>
       <h1> CounterApp </h1>
       <h2>{value}</h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
