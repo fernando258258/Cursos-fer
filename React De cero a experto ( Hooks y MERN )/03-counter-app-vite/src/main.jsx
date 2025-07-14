bedb21c4efb77591e39c7336a2fbefd3f13e64ea
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-13 22:30:23 -0400

    Colocar estilos de CSS

--- React De cero a experto ( Hooks y MERN )/03-counter-app-vite/src/main.jsx
+++ React De cero a experto ( Hooks y MERN )/03-counter-app-vite/src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import { App } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
 
+import "./styles.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
