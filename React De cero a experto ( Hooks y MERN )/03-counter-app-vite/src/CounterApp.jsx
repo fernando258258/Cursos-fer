2769784d9e9dd4684f7db893b9ff15de30a07607
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-14 10:47:03 -0400

    useState Hook

--- React De cero a experto ( Hooks y MERN )/03-counter-app-vite/src/CounterApp.jsx
+++ React De cero a experto ( Hooks y MERN )/03-counter-app-vite/src/CounterApp.jsx
@@ -1,14 +1,18 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
-  const handleAdd = (event) => {
-    console.log(event);
+  const [counter, setCounter] = useState(10);
+  const handleAdd = () => {
+    // console.log(event);
+    setCounter(counter + 1);
   };
 
   return (
     <>
       <h1> CounterApp </h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
       <button onClick={handleAdd}>+1</button>
     </>
   );
